32a38f903c4a090c0941e9b8c8ad8dda6b118664
Jeff Papiez <jpapiez@live.com>
2025-04-11 09:25:53 -0700

    Replace LD0 with LDO

--- src/data/steppers.ts
+++ src/data/steppers.ts
@@ -220,8 +220,8 @@ export const Steppers = z.array(Stepper).parse([
 		maxPeakCurrent: 1.68,
 	},
 	{
-		id: 'LD0-42STH40-1684CL350ET',
-		title: 'LD0-42STH40-1684CL350ET',
+		id: 'LDO-42STH40-1684CL350ET',
+		title: 'LDO-42STH40-1684CL350ET',
 		maxPeakCurrent: 1.68,
 		presets: [
 			{
@@ -247,8 +247,8 @@ export const Steppers = z.array(Stepper).parse([
 		],
 	},
 	{
-		id: 'LD0-42STH40-1684CL300ET',
-		title: 'LD0-42STH40-1684CL300ET',
+		id: 'LDO-42STH40-1684CL300ET',
+		title: 'LDO-42STH40-1684CL300ET',
 		maxPeakCurrent: 1.68,
 		presets: [
 			{
